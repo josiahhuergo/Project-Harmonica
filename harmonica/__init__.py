304a0a99b0588cc145e22f2a146dfee11a865f94
Josiah Huergo <joehuergo@gmail.com>
2026-04-18 23:30:42 -0400

    Submodule added

--- harmonica/__init__.py
+++ harmonica/__init__.py
@@ -1,3 +1,7 @@
 """Harmonica is a Python library for the generation, analysis and transformation of music theoretical objects."""
 
-from . import find, pitch, time, utility
+from .find import *
+from .pitch import *
+from .time import *
+from .utility import *
+from .velocity import *
